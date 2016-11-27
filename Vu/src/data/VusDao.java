7dc51fb03c0308e852484c39908de48efa52c838
Sarah Passantino <spassantino@yahoo.com>
2016-11-26 18:54:19 -0700

    Vus dao

---
+++ Vu/src/data/VusDao.java
@@ -0,0 +1,19 @@
+package data;
+
+import entities.Vu;
+
+public interface VusDao {
+
+	public int addYear();
+
+	public int removeYear();
+
+	public int editYear();
+
+	public Vu addVu();
+
+	public Vu removeVu();
+
+	public Vu editVu();
+
+}
