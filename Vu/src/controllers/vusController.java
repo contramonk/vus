a5afcbea15b89ea5c14b6c59a7926afa617a6c33
Sarah Passantino <spassantino@yahoo.com>
2016-11-26 19:45:04 -0700

    updates to vusdaoimpl

--- Vu/src/controllers/vusController.java
+++ Vu/src/controllers/vusController.java
@@ -13,66 +13,70 @@ import org.springframework.web.servlet.ModelAndView;
 import data.VuDao;
 import data.VusDao;
 import entities.Vu;
-@Repository 
+
+@Repository
 @SessionAttributes("")
 public class vusController {
 
-@Autowired
-private VusDao vusDao;
+	@Autowired
+	private VusDao vusDao;
 
-@RequestMapping("addYear.do") //for adding a year
-public ModelAndView addYear(@RequestParam("year") int year) {
-	ModelAndView mv = new ModelAndView();
-	mv.setViewName("vus.jsp");
-	mv.addObject(vusDao.addYear(year));
-	
-	return mv;
-}
+	@RequestMapping("addYear.do") // for adding a year
+	public ModelAndView addYear(@RequestParam("year") int year) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("vus.jsp");
+		mv.addObject(vusDao.addYear(year));
 
-@RequestMapping("editYear.do") //for editing a year
-public ModelAndView editYear(@RequestParam("year") int year) {
-	ModelAndView mv = new ModelAndView();
-	mv.setViewName("vus.jsp");
-	mv.addObject(vusDao.editYear(year));
-	
-	return mv;
-}
+		return mv;
+	}
 
-@RequestMapping("removeYear.do") //for removing a year
-public ModelAndView removeYear(@RequestParam("year") int year) {
-	ModelAndView mv = new ModelAndView();
-	mv.setViewName("vus.jsp");
-	mv.addObject(vusDao.removeYear(year));
-	
-	return mv;
-}
+	@RequestMapping("editYear.do") // for editing a year
+	public ModelAndView editYear(@RequestParam("year") int year) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("vus.jsp");
+		mv.addObject(vusDao.editYear(year));
 
-@RequestMapping("addVu.do") //for adding a Vu
-public ModelAndView addVu(@RequestParam("locationId") int locationId, @RequestParam("title") String title, 
-		@RequestParam("post") String post, @RequestParam("startDate")Date startDate, @RequestParam("endDate") Date endDate, @RequestParam("vu")Vu vu) {
-	ModelAndView mv = new ModelAndView();
-	mv.setViewName("vus.jsp");
-	mv.addObject("Vu", vusDao.addVu(vu));
-	
-	return mv;
-}
-@RequestMapping("editVu.do") //for editing a Vu
-public ModelAndView editVu(@RequestParam("locationId") int locationId, @RequestParam("title") String title, 
-		@RequestParam("post") String post, @RequestParam("startDate")Date startDate, @RequestParam("endDate") Date endDate, @RequestParam("vu")Vu vu) {
-	ModelAndView mv = new ModelAndView();
-	mv.setViewName("vus.jsp");
-	mv.addObject("Vu", vusDao.editVu(vu));
-	
-	return mv;
-}
-@RequestMapping("removeVu.do") //for removing a Vu
-public ModelAndView removeVu(@RequestParam("vu")Vu vu) {
-	ModelAndView mv = new ModelAndView();
-	mv.setViewName("vus.jsp");
-	mv.addObject("Vu", vusDao.removeVu(vu));
-	
-	return mv;
-}
+		return mv;
+	}
 
+	@RequestMapping("removeYear.do") // for removing a year
+	public ModelAndView removeYear(@RequestParam("year") int year) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("vus.jsp");
+		mv.addObject(vusDao.removeYear(year));
+
+		return mv;
+	}
+
+	@RequestMapping("addVu.do") // for adding a Vu
+	public ModelAndView addVu(@RequestParam("locationId") int locationId, @RequestParam("title") String title,
+			@RequestParam("post") String post, @RequestParam("startDate") Date startDate,
+			@RequestParam("endDate") Date endDate, @RequestParam("vu") Vu vu) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("vus.jsp");
+		mv.addObject("Vu", vusDao.addVu(vu));
+
+		return mv;
+	}
+
+	@RequestMapping("editVu.do") // for editing a Vu
+	public ModelAndView editVu(@RequestParam("locationId") int locationId, @RequestParam("title") String title,
+			@RequestParam("post") String post, @RequestParam("startDate") Date startDate,
+			@RequestParam("endDate") Date endDate, @RequestParam("vu") Vu vu) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("vus.jsp");
+		mv.addObject("Vu", vusDao.editVu(vu));
+
+		return mv;
+	}
+
+	@RequestMapping("removeVu.do") // for removing a Vu
+	public ModelAndView removeVu(@RequestParam("vu") Vu vu) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("vus.jsp");
+		mv.addObject("Vu", vusDao.removeVu(vu));
+
+		return mv;
+	}
 
 }
