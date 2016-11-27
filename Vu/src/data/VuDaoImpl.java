236803131cca9227135e3a03a36cd3ecf501b63b
Sarah Passantino <spassantino@yahoo.com>
2016-11-27 08:02:49 -0700

    recommit

--- Vu/src/data/VuDaoImpl.java
+++ Vu/src/data/VuDaoImpl.java
@@ -2,65 +2,69 @@ package data;
 
 import java.util.Date;
 
-import entities.Photo;
+import org.springframework.stereotype.Repository;
+
 import entities.Vu;
+//Added @Repository to DAO
+@Repository
+public class VuDaoImpl implements VuDAO {
 
-public class VuDaoImpl implements VuDao {
+//	@Override
+//	public Vu setTitle(Vu vu, String title) {
+//		vu.setTitle(title);  
+//		
+//		return vu; 
+//	}
+//	@Override	
+//	public Vu setStartDate(Vu vu, Date startDate){
+//		vu.setStartDate(startDate);
+//		
+//		return vu; 
+//	}
+//	@Override
+//	public Vu setEndDate(Vu vu, Date endDate){
+//		vu.setEndDate(endDate);
+//		
+//		return vu; 
+//	}
+//	@Override
+//	public Photo setPhoto(Photo photo, String imgUrl){
+//	
+//		//not sure how to access multiple photos from the data base
+//		
+//		return vu; 
+//	}
+//	@Override
+//	public Vu addPost (Vu vu, String post){
+//		vu.setPost(post);
+//		return vu; 
+//	}
+//	@Override
+//	public Address addAddress(Address add, String address){
+//		Address.setAddress; 
+//		
+//		return Address; 
+//	}
+//	@Override
+//	public Address addCity(Address address, String city){
+//		Address.setCity; 
+//		
+//		return address; 
+//	}
+//	@Override
+//	public Address addState(Address address, String state){
+//		Address.setState; 
+//		
+//		return address; 
+//	}
+//	@Override
+//	public Address addZip(Address address, int zip){
+//		Address.setZip; 
+//		
+//		return address; 
+//	}
 
-	@Override
-	public Vu setTitle(Vu vu, String title) {
-		vu.setTitle(title);  
-		
-		return vu; 
-	}
-		
-	public Vu setStartDate(Vu vu, Date startDate){
-		vu.setStartDate(startDate);
-		
-		return vu; 
-	}
 	
-	public Vu setEndDate(Vu vu, Date endDate){
-		vu.setEndDate(endDate);
-		
-		return vu; 
-	}
-	
-	public Photo setPhoto(Vu vu, String imgUrl){
-	
-		//not sure how to access multiple photos from the data base
-		
-		return vu; 
-	}
-	
-	public Vu addPost (Vu vu, String post){
-		vu.setPost(post);
-		return vu; 
-	}
-	
-	public Address addAddress(Address Address, String address){
-		Address.setAddress; 
-		
-		return Address; 
-	}
-	
-	public Address addCity(Address address, String city){
-		Address.setCity; 
-		
-		return address; 
-	}
-	
-	public Address addState(Address address, String state){
-		Address.setState; 
-		
-		return address; 
-	}
-	
-	public Address addZip(Address address, int zip){
-		Address.setZip; 
-		
-		return address; 
-	}
 	
 	
 }
