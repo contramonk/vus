94a6d3c8a6871dcc4d9325f762828783d73d21af
Jonathan Edwards <edwardjd@colorado.edu>
2016-11-27 12:57:44 -0700

    servlet-mapping fix

--- Vu/src/entities/Photo.java
+++ Vu/src/entities/Photo.java
@@ -2,6 +2,7 @@ package entities;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,14 +18,14 @@ public class Photo {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
-<<<<<<< HEAD
+
 	
 	@ManyToOne
     @JoinColumn(name = "vu_id")
     private List<Vu> vus;
-=======
+
 	@Column(name = "vu_id")
->>>>>>> c2b1f35c4b73dad5250a7742ad67654dda3a307f
+
 	private int vuId; 
 	private String url;
 	private String caption; 
