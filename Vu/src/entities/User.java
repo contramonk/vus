580189e529292eaabae78b6be1c9be5d857b0ef9
chrisbuttaro <chris.buttaro@gmail.com>
2016-11-28 10:18:14 -0700

    set up user class

--- Vu/src/entities/User.java
+++ Vu/src/entities/User.java
@@ -1,10 +1,19 @@
 package entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="users")
 public class User {
+	
+@Id
+@Column(name = "username")
+private String userName;
+private String password;
+@Column(name = "first_name")
+private String firstName;
+@Column(name = "last_name")
+private String lastName;
+private String enabled; 
 
 }
