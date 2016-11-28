4e33ec06527e66607e21990eedebd30c406f2695
chrisbuttaro <chris.buttaro@gmail.com>
2016-11-28 15:23:43 -0700

    updating sql database

--- Vu/src/data/VuDAO.java
+++ Vu/src/data/VuDAO.java
@@ -2,18 +2,20 @@ package data;
 
 import java.util.Date;
 
+import entities.Location;
+import entities.Photo;
 import entities.Vu;
 
 
 public interface VuDAO {
-//	public Vu setTitle(Vu vu, String title);
-//	public Vu setStartDate(Vu vu, Date startDate); 
-//	public Vu setEndDate(Vu vu, Date endDate); 
-//	public Photo setPhoto(Photo photo, String imgUrl);
-//	public Vu addPost (Vu vu, String post);
-//	public Address addAddress(Address Address, String address);
-//	public Address addCity(Address address, String city);
-//	public Address addState(Address address, String state);
-//	public Address addZip(Address address, int zip);
+	public Vu setTitle(Vu vu, String title);
+	public Vu setStartDate(Vu vu, Date startDate); 
+	public Vu setEndDate(Vu vu, Date endDate); 
+	public Photo setPhoto(Photo photo, String imgUrl);
+	public Vu addPost (Vu vu, String post);
+	public Location addAddress(Location location, String address);
+	public Location addCity(Location location, String city);
+	public Location addState(Location location, String state);
+	public Location addZip(Location location, int zip);
 
 }
--- Vu/src/data/VuDaoImpl.java
+++ Vu/src/data/VuDaoImpl.java
@@ -10,60 +10,60 @@ import entities.Vu;
 @Repository
 public class VuDaoImpl implements VuDAO {
 
-//	@Override
-//	public Vu setTitle(Vu vu, String title) {
-//		vu.setTitle(title);  
-//		
-//		return vu; 
-//	}
-//	@Override	
-//	public Vu setStartDate(Vu vu, Date startDate){
-//		vu.setStartDate(startDate);
-//		
-//		return vu; 
-//	}
-//	@Override
-//	public Vu setEndDate(Vu vu, Date endDate){
-//		vu.setEndDate(endDate);
-//		
-//		return vu; 
-//	}
-//	@Override
-//	public Photo setPhoto(Photo photo, String imgUrl){
-//	
-//		//not sure how to access multiple photos from the data base
-//		
-//		return vu; 
-//	}
-//	@Override
-//	public Vu addPost (Vu vu, String post){
-//		vu.setPost(post);
-//		return vu; 
-//	}
-//	@Override
-//	public Address addAddress(Address add, String address){
-//		Address.setAddress; 
-//		
-//		return Address; 
-//	}
-//	@Override
-//	public Address addCity(Address address, String city){
-//		Address.setCity; 
-//		
-//		return address; 
-//	}
-//	@Override
-//	public Address addState(Address address, String state){
-//		Address.setState; 
-//		
-//		return address; 
-//	}
-//	@Override
-//	public Address addZip(Address address, int zip){
-//		Address.setZip; 
-//		
-//		return address; 
-//	}
+	@Override
+	public Vu setTitle(Vu vu, String title) {
+		vu.setTitle(title);  
+		
+		return vu; 
+	}
+	@Override	
+	public Vu setStartDate(Vu vu, Date startDate){
+		vu.setStartDate(startDate);
+		
+		return vu; 
+	}
+	@Override
+	public Vu setEndDate(Vu vu, Date endDate){
+		vu.setEndDate(endDate);
+		
+		return vu; 
+	}
+	@Override
+	public Photo setPhoto(Photo photo, String imgUrl){
+	
+		//not sure how to access multiple photos from the data base
+		
+		return vu; 
+	}
+	@Override
+	public Vu addPost (Vu vu, String post){
+		vu.setPost(post);
+		return vu; 
+	}
+	@Override
+	public Address addAddress(Address add, String address){
+		Address.setAddress; 
+		
+		return Address; 
+	}
+	@Override
+	public Address addCity(Address address, String city){
+		Address.setCity; 
+		
+		return address; 
+	}
+	@Override
+	public Address addState(Address address, String state){
+		Address.setState; 
+		
+		return address; 
+	}
+	@Override
+	public Address addZip(Address address, int zip){
+		Address.setZip; 
+		
+		return address; 
+	}
 
 	
 	
